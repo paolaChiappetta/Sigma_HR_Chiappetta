7aa4386d7134a8a0e8e1bd6650ad02b6f5f90860
paola <chiappettapaola@gmail.com>
2022-05-03 10:33:26 -0300

    Carga de servicios con fetch() de un json

--- js/servicio.js
+++ js/servicio.js
@@ -49,3 +49,43 @@ addServiceButton[0].addEventListener("click", setVisibleAddServiceForm);
 function setVisibleAddServiceForm (){
     addServiceForm[0].style.display = 'block';
 }
+
+//carga de servicios por json
+
+const cargaServiciosJson = async () => {
+    const resp = await
+    fetch('../json/servicios.json')
+    const data = await resp.json()
+    console.log(data)
+    
+    //genero la clase contenedora 
+    let serviceContainer = document.createElement("div");
+    serviceContainer.className = "serviceSectionContainer";
+
+    data.forEach(servicio => {
+        //genero el recuadro del servicio
+        let box = document.createElement("div");
+        box.className = "box";
+        //genero el div que contiene al servicio
+        let service = document.createElement("div");
+        service.className = "service";
+        box.appendChild(service);
+        //genero img para el logo
+        let img = document.createElement("img"); 
+        let logo = (servicio.categoria == 1) ? 
+        "https://img.icons8.com/external-flaticons-lineal-color-flat-icons/60/000000/external-interviews-job-search-flaticons-lineal-color-flat-icons.png" :
+        (servicio.categoria == 2) ?
+        "https://img.icons8.com/dusk/64/000000/mind-map--v1.png" :
+        "https://img.icons8.com/external-justicon-lineal-color-justicon/60/000000/external-exam-back-to-school-justicon-lineal-color-justicon.png";
+        img.src = logo;
+        //service.appendChild(img);
+        service.innerHTML = `<img src=${logo}/>
+                            <h4>${servicio.nombre}</h4>
+                            <p>Valor: ${servicio.valor}</p>
+                            <button class="cartButton">Añadir al carrito</button>`
+        let hiringServiceContainer = document.getElementsByClassName("hiringServiceContainer");
+        serviceContainer.appendChild(box);
+        hiringServiceContainer[0].appendChild(serviceContainer);
+    });
+}    
+cargaServiciosJson()
