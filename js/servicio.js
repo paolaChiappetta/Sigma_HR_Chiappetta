cff44e8ad5525b7371239b55b67e1a7b791e370e
paola <chiappettapaola@gmail.com>
2022-05-15 14:38:52 -0300

    filtro de servicios por categoría

--- js/servicio.js
+++ js/servicio.js
@@ -6,63 +6,28 @@ class Servicio {
     }
 }
 
-//agregar al carrito
+let hiringServiceContainer = document.getElementsByClassName("hiringServiceContainer");
 
-let cartButton = document.getElementsByClassName("cartButton");
+let primerIngreso = true;
 
-for(let i = 0; i < cartButton.length; i++){
-    cartButton[i].addEventListener("click", agregarServicio);
-}
-
-async function agregarServicio (){
-    const { value: cantidad } = await Swal.fire({
-        title: 'Ingrese la cantidad que desea contratar',
-        input: 'number'
-      })
-
-    if(cantidad){
-        Swal.fire({
-            icon: 'success',
-            title: 'Felicitaciones!',
-            text: 'Servicio agregado correctamente'
-          })
-    }else{
-        Swal.fire({
-            icon: 'error',
-            title: 'No se ha agregado el servicio',
-            text: 'Debes seleccionar la cantidad para agregarlo'
-          })
-    }  
-    
-    //falta incorporar función para sumar en el carrito  
-}
-
-
-//carga servicios nuevos
-let addServiceButton = document.getElementsByClassName("addServiceButton");
-let addServiceForm = document.getElementsByClassName("addServiceForm");
-addServiceForm[0].style.display = 'none';
-
-
-addServiceButton[0].addEventListener("click", setVisibleAddServiceForm);
-
-function setVisibleAddServiceForm (){
-    addServiceForm[0].style.display = 'block';
-}
-
-//carga de servicios por json
-
-const cargaServiciosJson = async () => {
+//CARGA GENERAL DE TODOS LOS SERVICIOS DEL JSON - se reutiliza también para cargar servicios con filtro por categoría
+const cargaServiciosJson = async (filtroCategoria) => {
     const resp = await
     fetch('../json/servicios.json')
     const data = await resp.json()
-    console.log(data)
     
     //genero la clase contenedora 
     let serviceContainer = document.createElement("div");
     serviceContainer.className = "serviceSectionContainer";
 
-    data.forEach(servicio => {
+    let arrayFiltradoPorCategoria;
+    if(filtroCategoria != 0){
+        arrayFiltradoPorCategoria =  data.filter((servicio) => servicio.categoria == filtroCategoria);
+    }else{
+        arrayFiltradoPorCategoria = data;
+    }
+
+    arrayFiltradoPorCategoria.forEach(servicio => {
         //genero el recuadro del servicio
         let box = document.createElement("div");
         box.className = "box";
@@ -76,14 +41,52 @@ const cargaServiciosJson = async () => {
         (servicio.categoria == 2) ?
         "https://img.icons8.com/dusk/64/000000/mind-map--v1.png" :
         "https://img.icons8.com/external-justicon-lineal-color-justicon/60/000000/external-exam-back-to-school-justicon-lineal-color-justicon.png";
-        
+            
         service.innerHTML = `<img src=${logo}/>
                             <h4>${servicio.nombre}</h4>
                             <p>Valor: ${servicio.valor}</p>
                             <button class="cartButton">Añadir al carrito</button>`
-        let hiringServiceContainer = document.getElementsByClassName("hiringServiceContainer");
+            
         serviceContainer.appendChild(box);
         hiringServiceContainer[0].appendChild(serviceContainer);
     });
-}    
-cargaServiciosJson()
+}
+
+if(primerIngreso){
+    cargaServiciosJson(0);
+}
+
+//FILTRO DE VISTA DE SERVICIOS POR CATEGORÍA
+
+let serviceFilterForm = document.getElementsByClassName("serviceFilterForm");
+
+serviceFilterForm[0].addEventListener("submit", filtrarServiciosPorCategoria);
+
+function filtrarServiciosPorCategoria(e){
+    e.preventDefault();
+    let checked;
+    let  chequeado = document.getElementsByName("categoriaFiltro");
+    for(let i=0; i<chequeado.length; i++){
+        if(chequeado[i].checked){
+            checked = chequeado[i].value;
+        }
+    }
+    hiringServiceContainer[0].innerHTML = "";
+    primerIngreso = false;
+    cargaServiciosJson(checked);
+}
+
+
+//CARGA DE NUEVOS SERVICIOS POR EL ADMIN
+let addServiceButton = document.getElementsByClassName("addServiceButton");
+let addServiceForm = document.getElementsByClassName("addServiceForm");
+addServiceForm[0].style.display = 'none';
+
+
+addServiceButton[0].addEventListener("click", setVisibleAddServiceForm);
+
+function setVisibleAddServiceForm (){
+    addServiceForm[0].style.display = 'block';
+}
+
+
